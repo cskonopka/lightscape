eee352f2411474b826b441c6696a1f8a8bf856c6
Christopher Konopka <cskonopka@gmail.com>
2025-07-27 19:36:04 -0400

    updated for v2

--- shapes.js
+++ shapes.js
@@ -1,190 +1,102 @@
-function drawFullPlane(pg, r, g, b, a) {
-  pg.fill(r, g, b, a);
+let shapeOptions = [
+  'Circle', 'Square', 'Triangle',
+  'Vertical Panel', 'Horizontal Panel',
+  'Diagonal Panel', 'Diagonal Panel (Flipped)',
+  'Polygon', 'Pentagon', 'Hexagon', 'Heptagon', 'Octagon', 'Nonagon', 'Rhombus'
+];
+
+function drawShapeSystem(pg, shapeType, col, alpha, size, invert, offsetX, offsetY, rotation) {
+  pg.push();
+  pg.translate(pg.width / 2 + offsetX, pg.height / 2 + offsetY);
+  pg.rotate(rotation);
+  pg.fill(red(col), green(col), blue(col), alpha);
   pg.noStroke();
-  pg.rect(0, 0, 400, 400);
+
+  if (invert) {
+    pg.erase();
+    drawShape(pg, shapeType, 0, 0, size);
+    pg.noErase();
+    pg.fill(red(col), green(col), blue(col), alpha);
+    pg.rectMode(CENTER);
+    pg.rect(0, 0, pg.width * 2, pg.height * 2);
+  } else {
+    drawShape(pg, shapeType, 0, 0, size);
+  }
+
+  pg.pop();
 }
 
-function drawCircle(pg, r, g, b, a) {
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.ellipse(200, 200, 320, 320);
+function drawShape(pg, shapeType, x, y, size) {
+  switch (shapeType) {
+    case 'Circle':
+      pg.ellipse(x, y, size);
+      break;
+    case 'Square':
+      pg.rectMode(CENTER);
+      pg.rect(x, y, size, size);
+      break;
+    case 'Triangle':
+      const h = (sqrt(3) / 2) * size;
+      pg.triangle(x, y - h / 2, x - size / 2, y + h / 2, x + size / 2, y + h / 2);
+      break;
+    case 'Vertical Panel':
+      pg.rectMode(CENTER);
+      pg.rect(x, y, size / 3, size);
+      break;
+    case 'Horizontal Panel':
+      pg.rectMode(CENTER);
+      pg.rect(x, y, size, size / 3);
+      break;
+    case 'Diagonal Panel':
+      pg.push();
+      pg.rotate(PI / 4);
+      pg.rectMode(CENTER);
+      pg.rect(x, y, size, size / 4);
+      pg.pop();
+      break;
+    case 'Diagonal Panel (Flipped)':
+      pg.push();
+      pg.rotate(-PI / 4);
+      pg.rectMode(CENTER);
+      pg.rect(x, y, size, size / 4);
+      pg.pop();
+      break;
+    case 'Polygon':
+      drawRegularPolygon(pg, x, y, size / 2, 6);
+      break;
+    case 'Pentagon':
+      drawRegularPolygon(pg, x, y, size / 2, 5);
+      break;
+    case 'Hexagon':
+      drawRegularPolygon(pg, x, y, size / 2, 6);
+      break;
+    case 'Heptagon':
+      drawRegularPolygon(pg, x, y, size / 2, 7);
+      break;
+    case 'Octagon':
+      drawRegularPolygon(pg, x, y, size / 2, 8);
+      break;
+    case 'Nonagon':
+      drawRegularPolygon(pg, x, y, size / 2, 9);
+      break;
+    case 'Rhombus':
+      pg.quad(
+        x, y - size / 2,
+        x + size / 3, y,
+        x, y + size / 2,
+        x - size / 3, y
+      );
+      break;
+  }
 }
 
-function drawCircleInverse(pg, r, g, b, a) {
-  let mask = pg.createGraphics(400, 400);
-  mask.fill(255);
-  mask.noStroke();
-  mask.rect(0, 0, 400, 400);
-  mask.erase();
-  mask.ellipse(200, 200, 320, 320);
-  mask.noErase();
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.image(mask, 0, 0);
-}
-
-function drawTriangle(pg, r, g, b, a) {
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.triangle(200, 40, 360, 360, 40, 360);
-}
-
-function drawTriangleInverse(pg, r, g, b, a) {
-  let mask = pg.createGraphics(400, 400);
-  mask.fill(255);
-  mask.noStroke();
-  mask.rect(0, 0, 400, 400);
-  mask.erase();
-  mask.triangle(200, 40, 360, 360, 40, 360);
-  mask.noErase();
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.image(mask, 0, 0);
-}
-
-function drawRectangle(pg, r, g, b, a) {
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.rectMode(pg.CENTER);
-  pg.rect(200, 200, 320, 320);
-}
-
-function drawRectangleInverse(pg, r, g, b, a) {
-  let mask = pg.createGraphics(400, 400);
-  mask.fill(255);
-  mask.noStroke();
-  mask.rect(0, 0, 400, 400);
-  mask.erase();
-  mask.rectMode(mask.CENTER);
-  mask.rect(200, 200, 320, 320);
-  mask.noErase();
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.image(mask, 0, 0);
-}
-
-function drawStar(pg, r, g, b, a) {
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  let angle = pg.TWO_PI / 5;
-  let halfAngle = angle / 2;
+function drawRegularPolygon(pg, x, y, radius, npoints) {
+  let angle = TWO_PI / npoints;
   pg.beginShape();
-  for (let a = -pg.PI / 2; a < pg.TWO_PI - pg.PI / 2; a += angle) {
-    let sx = 200 + pg.cos(a) * 160;
-    let sy = 200 + pg.sin(a) * 160;
-    pg.vertex(sx, sy);
-    sx = 200 + pg.cos(a + halfAngle) * 80;
-    sy = 200 + pg.sin(a + halfAngle) * 80;
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius;
+    let sy = y + sin(a) * radius;
     pg.vertex(sx, sy);
   }
-  pg.endShape(pg.CLOSE);
-}
-
-function drawStarInverse(pg, r, g, b, a) {
-  let mask = pg.createGraphics(400, 400);
-  mask.fill(255);
-  mask.noStroke();
-  mask.rect(0, 0, 400, 400);
-  mask.erase();
-  let angle = mask.TWO_PI / 5;
-  let halfAngle = angle / 2;
-  mask.beginShape();
-  for (let a = -mask.PI / 2; a < mask.TWO_PI - mask.PI / 2; a += angle) {
-    let sx = 200 + mask.cos(a) * 160;
-    let sy = 200 + mask.sin(a) * 160;
-    mask.vertex(sx, sy);
-    sx = 200 + mask.cos(a + halfAngle) * 80;
-    sy = 200 + mask.sin(a + halfAngle) * 80;
-    mask.vertex(sx, sy);
-  }
-  mask.endShape(mask.CLOSE);
-  mask.noErase();
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.image(mask, 0, 0);
-}
-
-function drawDiamond(pg, r, g, b, a) {
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.beginShape();
-  pg.vertex(200, 40);
-  pg.vertex(360, 200);
-  pg.vertex(200, 360);
-  pg.vertex(40, 200);
-  pg.endShape(pg.CLOSE);
-}
-
-function drawDiamondInverse(pg, r, g, b, a) {
-  let mask = pg.createGraphics(400, 400);
-  mask.fill(255);
-  mask.noStroke();
-  mask.rect(0, 0, 400, 400);
-  mask.erase();
-  mask.beginShape();
-  mask.vertex(200, 40);
-  mask.vertex(360, 200);
-  mask.vertex(200, 360);
-  mask.vertex(40, 200);
-  mask.endShape(mask.CLOSE);
-  mask.noErase();
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.image(mask, 0, 0);
-}
-
-function drawHalfPlane(pg, r, g, b, a) {
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.beginShape();
-  pg.vertex(0, 0);
-  pg.vertex(200, 0);
-  pg.vertex(200, 400);
-  pg.vertex(0, 400);
-  pg.endShape(pg.CLOSE);
-}
-
-function drawHalfPlaneInverse(pg, r, g, b, a) {
-  let mask = pg.createGraphics(400, 400);
-  mask.fill(255);
-  mask.noStroke();
-  mask.rect(0, 0, 400, 400);
-  mask.erase();
-  mask.beginShape();
-  mask.vertex(0, 0);
-  mask.vertex(200, 0);
-  mask.vertex(200, 400);
-  mask.vertex(0, 400);
-  mask.endShape(mask.CLOSE);
-  mask.noErase();
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.image(mask, 0, 0);
-}
-
-function drawDiagonalLinear(pg, r, g, b, a) {
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.beginShape();
-  pg.vertex(0, 400);
-  pg.vertex(0, 0);
-  pg.vertex(400, 400);
-  pg.endShape(pg.CLOSE);
-}
-
-function drawDiagonalLinearInverse(pg, r, g, b, a) {
-  let mask = pg.createGraphics(400, 400);
-  mask.fill(255);
-  mask.noStroke();
-  mask.rect(0, 0, 400, 400);
-  mask.erase();
-  mask.beginShape();
-  mask.vertex(0, 400);
-  mask.vertex(0, 0);
-  mask.vertex(400, 400);
-  mask.endShape(mask.CLOSE);
-  mask.noErase();
-  pg.fill(r, g, b, a);
-  pg.noStroke();
-  pg.image(mask, 0, 0);
+  pg.endShape(CLOSE);
 }
